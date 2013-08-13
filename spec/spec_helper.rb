d3bf59edf858d09fd6f893fc5b6af0c0375c57a6
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-13 17:57:30 +0300

    Use factory girl shorthands

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,6 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = "random"
+
+  config.include FactoryGirl::Syntax::Methods
 end
