4d6b058bd40aeebc23029699edb940865caa1de6
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-14 14:44:49 +0300

    Check imported files for existence

--- app/tasks/import_contacts.rb
+++ app/tasks/import_contacts.rb
@@ -1,6 +1,8 @@
 class ImportContacts
   def initialize(file_path)
-    @file_path = file_path
+    @file_path = Pathname.new(file_path)
+
+    raise ArgumentError.new("#{file_path} does not exist") unless @file_path.exist?
   end
 
   def import
--- spec/tasks/import_contacts_spec.rb
+++ spec/tasks/import_contacts_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
+require 'fileutils'
 
-describe ImportContacts do
+describe ImportContacts, with_fakefs: true do
   describe '#import' do
-    it 'imports contact entries' do
-      pending
+    context 'provided path does not exist' do
+      it 'raises ArgumentError' do
+        expect{
+          ImportContacts.new('non-existent-path')
+        }.to raise_error ArgumentError
+      end
+    end
+
+    context 'provided path exists' do
+      let(:file_name) { 'contacts.csv' }
+
+      before { FileUtils.touch(file_name)  }
+
+      it 'does not raise ArgumentError' do
+        expect{
+          ImportContacts.new(file_name)
+        }.not_to raise_error
+      end
     end
   end
 end
