57ff5854fd473129a495fe897c52b3358c82166b
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-15 16:27:59 +0300

    Extract to separate methods for clarity

--- app/tasks/import_contacts/contact_record_builder.rb
+++ app/tasks/import_contacts/contact_record_builder.rb
@@ -22,12 +22,30 @@ class ImportContacts
     end
 
     def build
-      @contact_record.websites = WebsiteBuilder.build(@contact_record, attributes).select(&:valid?)
-      @contact_record.email_addresses = EmailAddressBuilder.build(@contact_record, attributes).select(&:valid?)
-      @contact_record.post_addresses = PostAddressBuilder.build(@contact_record, attributes).select(&:valid?)
-      @contact_record.numbers = NumberBuilder.build(@contact_record, attributes).select(&:valid?)
+      @contact_record.websites = website_records
+      @contact_record.email_addresses = email_address_records
+      @contact_record.post_addresses = post_address_records
+      @contact_record.numbers = number_records
 
       @contact_record
+    end
+
+    private
+
+    def website_records
+      WebsiteBuilder.build(@contact_record, attributes).select(&:valid?)
+    end
+
+    def email_address_records
+      EmailAddressBuilder.build(@contact_record, attributes).select(&:valid?)
+    end
+
+    def post_address_records
+      PostAddressBuilder.build(@contact_record, attributes).select(&:valid?)
+    end
+
+    def number_records
+      NumberBuilder.build(@contact_record, attributes).select(&:valid?)
     end
   end
 end
