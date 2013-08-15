66824243e18f2b21f9449bf059f5af7848a771bc
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-15 12:21:16 +0300

    Validate presence of email if it is not more info address

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  it { should validate_presence_of :contact_record }
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :email }
+  describe "validations" do
+    it { should validate_presence_of :contact_record }
+    it { should validate_presence_of :title }
+
+    context 'more info text present' do
+      subject { EmailAddress.new(more_info: 'text') }
+
+      it { should_not validate_presence_of :email }
+    end
+
+    context 'more info text missing' do
+      subject { EmailAddress.new(more_info: nil) }
+
+      it { should validate_presence_of :email }
+    end
+  end
 end
