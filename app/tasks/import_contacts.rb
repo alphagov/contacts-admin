51ef389fceb8468dc43a1fbd658c50257c8bfb7f
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-15 15:29:57 +0300

    Extract record building to separate classes

--- app/tasks/import_contacts.rb
+++ app/tasks/import_contacts.rb
@@ -1,6 +1,85 @@
 require 'csv'
 
 class ImportContacts
+  class WebsiteRecordBuilder
+    def self.build(attributes)
+      [{
+         title: attributes['ogtitle1'],
+         description: attributes['ogtag1'],
+         link: attributes['ogurl1']
+       },{
+         title: attributes['ogtitle2'],
+         description: attributes['ogtag2'],
+         link: attributes['ogurl2']
+       },{
+         title: attributes['ogmoreinfourltitle'],
+         description: attributes['ogmoreinfourltag'],
+         link: attributes['ogmoreinfourl'],
+         more_info: attributes['ogmoreinfo']
+       }].map { |website_attributes|
+         Website.new(website_attributes)
+       }
+    end
+  end
+
+  class NumberBuilder
+    def self.build(attributes)
+      [{
+         title: attributes['telephonename'],
+         description: attributes['phonetexthead'],
+         open_hours: attributes['phoneopenhours'],
+         number: attributes['telephone']
+      },{
+         title: attributes['telephonename2'],
+         description: attributes['phonetexthead2'],
+         open_hours: attributes['phoneopenhours2'],
+         number: attributes['telephone2']
+      }].map { |number_attributes|
+        Number.new(number_attributes)
+      }
+    end
+  end
+
+  class PostAddressBuilder
+    def self.build(attributes)
+      [{
+         description: attributes['postaddresstag1'],
+         address: attributes['postaddress1']
+       },{
+         description: attributes['postaddresstag2'],
+         address: attributes['postaddress2']
+       },{
+         description: attributes['postaddresstag3'],
+         address: attributes['postaddress3']
+       }].map { |post_address_attributes|
+         PostAddress.new(post_address_attributes)
+       }
+    end
+  end
+
+  class EmailAddressBuilder
+    def self.build(attributes)
+      [{
+         title: attributes['emailtitle1'],
+         description: attributes['emailtag1'],
+         link: attributes['emailurl1'],
+         email: attributes['emailaddress1']
+       },{
+         title: attributes['emailtitle2'],
+         description: attributes['emailtag2'],
+         link: attributes['emailurl2'],
+         email: attributes['emailaddress2']
+       },{
+         title: attributes['emailmoreinfourltitle'],
+         description: attributes['emailmoreinfourltag'],
+         link: attributes['emailmoreinfourl'],
+         more_info: attributes['emailmoreinfo']
+       }].map { |email_address_attributes|
+         EmailAddress.new(email_address_attributes)
+       }
+    end
+  end
+
   class ContactRecordBuilder
     def self.build(attributes)
       new(attributes).build
@@ -18,80 +97,16 @@ class ImportContacts
     def contact_attributes
       {
         description: attributes['description'],
-        keywords: attributes.fetch('keywords', '').split(","),
+        keywords: attributes.fetch('keywords', '').to_s.split(","),
         contact_type: ContactType.find_by_title(attributes['clustergroup'])
       }
     end
 
-    def website_attributes
-      [{
-         title: attributes['ogtitle1'],
-         description: attributes['ogtag1'],
-         link: attributes['ogurl1']
-       },{
-         title: attributes['ogtitle2'],
-         description: attributes['ogtag2'],
-         link: attributes['ogurl2']
-       },{
-         title: attributes['ogmoreinfourltitle'],
-         description: attributes['ogmoreinfourltag'],
-         link: attributes['ogmoreinfourl'],
-         more_info: attributes['ogmoreinfo']
-       }]
-    end
-
-    def email_address_attributes
-      [{
-         title: attributes['emailtitle1'],
-         description: attributes['emailtag1'],
-         link: attributes['emailurl1'],
-         email: attributes['emailaddress1']
-       },{
-         title: attributes['emailtitle2'],
-         description: attributes['emailtag2'],
-         link: attributes['emailurl2'],
-         email: attributes['emailaddress2']
-       },{
-         title: attributes['emailmoreinfourltitle'],
-         description: attributes['emailmoreinfourltag'],
-         link: attributes['emailmoreinfourl'],
-         more_info: attributes['emailmoreinfo']
-       }]
-    end
-
-    def post_address_attributes
-      [{
-         description: attributes['postaddresstag1'],
-         address: attributes['postaddress1']
-       },{
-         description: attributes['postaddresstag2'],
-         address: attributes['postaddress2']
-       },{
-         description: attributes['postaddresstag3'],
-         address: attributes['postaddress3']
-       },{
-       }]
-    end
-
-    def number_attributes
-      [{
-         title: attributes['telephonename'],
-         description: attributes['phonetexthead'],
-         open_hours: attributes['phoneopenhours'],
-         number: attributes['telephone']
-      },{
-         title: attributes['telephonename2'],
-         description: attributes['phonetexthead2'],
-         open_hours: attributes['phoneopenhours2'],
-         number: attributes['telephone2']
-      }]
-    end
-
     def build
-      @contact_record.websites.build(website_attributes)
-      @contact_record.email_addresses.build(email_address_attributes)
-      @contact_record.post_addresses.build(post_address_attributes)
-      @contact_record.numbers.build(number_attributes)
+      @contact_record.websites = WebsiteRecordBuilder.build(attributes)
+      @contact_record.email_addresses = EmailAddressBuilder.build(attributes)
+      @contact_record.post_addresses = PostAddressBuilder.build(attributes)
+      @contact_record.numbers = NumberBuilder.build(attributes)
 
       @contact_record
     end
