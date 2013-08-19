353e6c31c69e4278741ab52ae278f82c84072b36
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-19 16:47:17 +0300

    Show email,number, and address counts associated to ContactRecord

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,5 +1,5 @@
 class EmailAddress < ActiveRecord::Base
-  belongs_to :contact_record, inverse_of: :email_addresses
+  belongs_to :contact_record, inverse_of: :email_addresses, counter_cache: true
 
   validates :contact_record, presence: true
   validates :title, presence: true
