b82ec822fb8118d2f95c446ecc09371bfb64c21a
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-21 15:12:39 +0300

    Abstract more info record controllers
    They are doing exactly the same, just the name differs

--- app/controllers/admin/contact_records/more_info_email_addresses_controller.rb
+++ app/controllers/admin/contact_records/more_info_email_addresses_controller.rb
@@ -1,35 +1,10 @@
 module Admin
   module ContactRecords
     class MoreInfoEmailAddressesController < Admin::ContactRecords::MoreInfoRecordController
-      def new
-        @more_info_email_address = contact_record.build_more_info_email_address
-      end
+      private
 
-      def create
-        @more_info_email_address = contact_record.build_more_info_email_address(more_info_attributes)
-
-        if @more_info_email_address.save
-          redirect_to [:edit, :admin, contact_record], notice: 'More Info Website record successfully created'
-        else
-          render :new
-        end
-      end
-
-      def edit
-        @more_info_email_address = contact_record.more_info_email_address
-      end
-
-      def update
-        @more_info_email_address = contact_record.more_info_email_address
-
-        if @more_info_email_address.update_attributes(more_info_attributes)
-          redirect_to [:edit, :admin, contact_record], notice: 'More Info Website record successfully updated'
-        else
-          render :new
-        end
-      end
-
-      def destroy
+      def more_info_record_type
+        :more_info_email_address
       end
     end
   end
--- app/controllers/admin/contact_records/more_info_record_controller.rb
+++ app/controllers/admin/contact_records/more_info_record_controller.rb
@@ -3,15 +3,55 @@ module Admin
     class MoreInfoRecordController < AdminController
       expose(:contact_record)
 
-      protected
+      def new
+        @more_info_record = build_more_info_record
+      end
+
+      def create
+        @more_info_record = build_more_info_record(more_info_attributes)
+
+        if @more_info_record.save
+          redirect_to [:edit, :admin, contact_record], notice: 'More Info record successfully created'
+        else
+          render :new
+        end
+      end
+
+      def edit
+        @more_info_record = fetch_more_info_record
+      end
+
+      def update
+        @more_info_record = fetch_more_info_record
+
+        if @more_info_record.update_attributes(more_info_attributes)
+          redirect_to [:edit, :admin, contact_record], notice: 'More Info record successfully updated'
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def build_more_info_record(attributes = {})
+        contact_record.send(:"build_#{more_info_record_type}", attributes)
+      end
+
+      def fetch_more_info_record
+        contact_record.send(more_info_record_type)
+      end
 
       def more_info_attributes
-        params.require(:more_info_email_address).permit(
+        params.require(more_info_record_type).permit(
           :description,
           :url_title,
           :url_description,
           :url
         )
+      end
+
+      def more_info_record_type
+        raise NotImplementedError.new("Should be overriden by inheriting classes")
       end
     end
   end
