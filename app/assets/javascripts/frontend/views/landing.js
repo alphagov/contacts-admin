8b4a89e7374cf4efc97d9c1bbe4ab6e21dafbfaf
Michael Ly <michael@bitzesty.com>
2013-08-22 13:44:09 +0100

    moved landing page js into the proper folder

---
+++ app/assets/javascripts/frontend/views/landing.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $("#commonly-asked-questions .js-chosen-select").chosen().change(function(){
+    update_common_questions($(this).val());
+  });
+  $("#commonly-asked-questions select").change(function(){
+    update_common_questions($(this).val());
+  });
+
+  update_common_questions = function(section){
+    console.log(section)
+    $(".common-sections-active").removeClass("common-sections-active");
+    $(section).addClass("common-sections-active");
+  }
+})(jQuery);
