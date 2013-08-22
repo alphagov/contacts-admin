06ce01c3e70a77c9e6b8c36a9cd5c358c56c650f
Michael Ly <michael@bitzesty.com>
2013-08-22 11:04:07 +0100

    added helpers and related images. Also styled the landing page minus the dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def page_title(*title_parts)
+    if title_parts.any?
+      title_parts.push("Admin") if params[:controller] =~ /^admin\//
+      title_parts.push("GOV.UK")
+      @page_title = title_parts.reject { |p| p.blank? }.join(" - ")
+    else
+      @page_title
+    end
+  end
+
+  def meta_description_tag
+    tag :meta, name: 'description', content: @meta_description
+  end
+
+  def page_class(css_class)
+    content_for(:page_class, css_class)
+  end
 end
