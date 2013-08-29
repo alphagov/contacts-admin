4aa4b92244b192ca98a178516f361eccdbb1e54e
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-29 13:21:21 +0300

    Refactor javascript on contact group listing page

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -1,3 +1,7 @@
 //= require govuk_toolkit
 //= require chosen.jquery.min
 //= require_tree ./frontend
+
+$(function(){
+  GOVUK.hmrc_contacts.onLoad();
+});
---
+++ app/assets/javascripts/frontend/hmrc_contacts.js
@@ -0,0 +1,78 @@
+/*global $ */
+/*jslint
+ white: true */
+
+"use strict";
+
+/**
+ * @name GOVUK
+ * @namespace
+ * @description Global namespace for gov.uk
+ * @requires jquery 1.6.2
+*/
+var GOVUK = GOVUK || {};
+
+/**
+  @name GOVUK.hmrc_contacts
+  @memberOf GOVUK
+  @namespace
+  @description A set of methods for handling behaviours with HMRC Contacts app functionality
+  @requires jquery 1.6.2
+*/
+GOVUK.hmrc_contacts = {
+  /**
+    @name GOVUK.hmrc_contacts.contactGroupList
+    @object
+    @description container for contact group list behaviour
+  */
+  contactGroupList : {
+      /**
+        @name GOVUK.hmrc_contacts.toggledControl.initialize
+        @function
+        @description initializes namespace
+      */
+      toggledControl: {
+        initialize: function(control) {
+          var $control = $(control),
+              $controlledElement = $($control.data('expand')),
+              $container = $control.closest('.department');
+
+          // hide all expandables unless asked to open
+          if (window.location.hash != $control.attr('href')) {
+            $controlledElement.addClass('js-hidden');
+            $container.addClass('js-hiding-children');
+          }
+
+          $control.on('click', function (e) {
+            var isOpen = $control.text().indexOf("view") != -1;
+
+            $control.text((isOpen) ? 'hide all' : 'view all');
+
+            $controlledElement.toggleClass('js-hidden');
+            $container.toggleClass('js-hiding-children');
+          });
+        }
+      },
+      /**
+        @name GOVUK.contactGroupList.initialize
+        @function
+        @description initializes namespace
+      */
+      initialize : function () {
+        var contactGroupToggle = '[data-expand]',
+            namespace = this;
+
+        $(contactGroupToggle).each(function(idx, element){
+          namespace.toggledControl.initialize(element);
+        })
+      }
+  },
+  /**
+    @name onLoad
+    @function
+    @description behaviour init point
+  */
+  onLoad : function () {
+    this.contactGroupList.initialize();
+  }
+};
