b2dc6d8961a0926ccf15219b95288d0c42176966
Michael Ly <michael@bitzesty.com>
2013-08-29 10:52:47 +0100

    Fixed contact groups page styling, added JS to toggle hide/show and renamed file to contact_groups.scss for consistency

--- app/assets/javascripts/frontend/common.js
+++ app/assets/javascripts/frontend/common.js
@@ -1,3 +1,10 @@
 (function ($) {
   $(".js-chosen-select").chosen();
+
+  //toggle hide class
+  $(".js-hide-link").click(function(event){
+    event.preventDefault();
+
+    $(this).closest(".js-hide-container").toggleClass("js-hide");
+  })
 })(jQuery);
