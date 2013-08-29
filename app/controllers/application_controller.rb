dc4812b060b68f70e8272101842facc65c84bcb6
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-29 16:12:02 +0300

    Allow skipping slimmer with ?skip_slimmer=1 param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  after_filter :skip_slimmer
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   def skip_slimmer
-    response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
+    response.headers[Slimmer::Headers::SKIP_HEADER] = "true" if params[:skip_slimmer]
   end
 end
