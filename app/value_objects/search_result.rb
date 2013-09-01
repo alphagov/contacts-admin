45f5973d585a631bfa667477582f41d059f3d82d
Matthew Ford <matt@bitzesty.com>
2013-09-01 23:01:00 +0100

    Fix rummager results to use gov.uk as the root domain
    While we are in testing

--- app/value_objects/search_result.rb
+++ app/value_objects/search_result.rb
@@ -8,4 +8,8 @@ class SearchResult
   attribute :link, String
   attribute :indexable_content, String
   attribute :es_score, Float
+
+  def govuk_link
+    ["https://www.gov.uk", link].join
+  end
 end
