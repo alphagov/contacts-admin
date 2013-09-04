b9c97bc54d4ce64099bea05f14dd1353ed719210
Michael Ly <michael@bitzesty.com>
2013-09-04 10:52:26 +0100

    Admin: List questions by contact group to mimic frontend

--- app/assets/javascripts/admin/common.js
+++ app/assets/javascripts/admin/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $(".js-chosen-select").chosen();
+
+  //expand questions accordion
+  $(".js-expand-link").click(function(){
+    $(this).closest(".js-expand-list").toggleClass("js-expanded");
+  })
 });
