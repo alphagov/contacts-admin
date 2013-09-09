a6e2e62c9f66378d3de06cf9909930fb9025fecb
Michael Ly <michael@bitzesty.com>
2013-09-09 17:30:11 +0100

    Made the admin questions hidden via JS initially

--- app/assets/javascripts/admin/common.js
+++ app/assets/javascripts/admin/common.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $(".js-chosen-select").chosen();
 
   //expand questions accordion
+  $(".js-expand-list").addClass("js-hidden");
   $(".js-expand-link").click(function(){
-    $(this).closest(".js-expand-list").toggleClass("js-expanded");
+    $(this).closest(".js-expand-list").toggleClass("js-hidden");
   })
 });
