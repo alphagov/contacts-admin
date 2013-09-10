b8ba277fd699cf2456db8fc4460b12a561f0f78a
Saulius Grigaliunas <saulius@ninja.lt>
2013-09-10 14:52:50 +0300

    Move ContactGroups#index to Contacts#index
    Fixes #34
    
    Also uses Rails 4 finders, #find_by!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,40 @@
 class ContactsController < ApplicationController
   expose(:department) {
-    Department.find_by_slug!(params[:department_id])
+    Department.find_by!(slug: params[:department_id])
   }
+
   expose(:contact) {
-    department.contacts.find_by_slug!(params[:id]).decorate
+    department.contacts.find_by!(slug: params[:id]).decorate
   }
+
+  expose(:most_popular_contact_groups) {
+    department.
+    contact_groups.
+    most_popular.
+    with_contacts.
+    by_title.
+    decorate
+  }
+
+  expose(:ungrouped_contacts) {
+    department.
+    contacts.
+    ungrouped.
+    for_listing.
+    decorate
+  }
+
+  expose(:contact_groups) {
+    department.
+    contact_groups.
+    with_contacts.
+    except_most_popular.
+    by_title.
+    decorate
+  }
+
+  def index
+  end
 
   def show
   end
