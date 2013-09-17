2226d1412f61bc872db7ec07ee8d3c234a787da6
Matthew Ford <matt@bitzesty.com>
2013-09-17 16:28:36 +0100

    Fix missing method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
 
-  #
+
   # GDS::SSO::User overwrites for Rails 4
   def self.find_for_gds_oauth(auth_hash)
-    user_params = user_params_from_auth_hash(auth_hash)
+    user_params = GDS::SSO::User.user_params_from_auth_hash(auth_hash.to_hash)
 
     # update details of existing user
     if user = where(uid: auth_hash["uid"]).first
