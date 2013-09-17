515d3cd2cf49ca2d60ee422847983e3ed0adc313
Matthew Ford <matt@bitzesty.com>
2013-09-17 16:20:39 +0100

    Rails 4 uses strong params so overrite User.find_for_gds_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
+
+  #
+  # GDS::SSO::User overwrites for Rails 4
+  def self.find_for_gds_oauth(auth_hash)
+    user_params = user_params_from_auth_hash(auth_hash)
+
+    # update details of existing user
+    if user = where(uid: auth_hash["uid"]).first
+      user.update_attributes(user_params)
+    else # Create a new user.
+      create!(user_params)
+    end
+  end
 end
