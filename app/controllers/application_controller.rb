2beb90c8c348ceadcb2857da399c618780a98231
Matthew Ford <matt@bitzesty.com>
2013-09-17 15:57:06 +0100

    Fix SSO and rename development skip slimmer filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
+  include GDS::SSO::ControllerMethods
   include Slimmer::Headers
   include Slimmer::Template
 
   protect_from_forgery with: :exception
 
-  after_filter :skip_slimmer
+  after_filter :dev_skip_slimmer
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
-  def skip_slimmer
+  def dev_skip_slimmer
     response.headers[Slimmer::Headers::SKIP_HEADER] = "true" if params[:skip_slimmer]
   end
 end
