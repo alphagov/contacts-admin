c6b878ddc0d2000dd3c2d65d62f2d370c230ff9b
Matthew Ford <matt@bitzesty.com>
2013-09-26 12:47:43 +0100

    Use the contacts module to specify the worldwide_api
    Instead of a global variable, so that we can switch it out in testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Contacts::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.after_initialize do
+    Contacts.worldwide_api = GdsApi::Worldwide.new("https://www.gov.uk")
+  end
 end
