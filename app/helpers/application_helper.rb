6e95047f8472617287415c01bf305d9583b55fc1
Matthew Ford <matt@bitzesty.com>
2013-10-15 15:53:31 +0100

    Convert from markdown to govspeak
    Also removing previews with marked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,17 +17,7 @@ module ApplicationHelper
     content_for(:page_class, css_class)
   end
 
-  def markdown(markdown_text, renderer = markdown_renderer)
-    renderer.render(markdown_text).html_safe if markdown_text
-  end
-
-  private
-
-  def markdown_renderer
-    @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::XHTML.new(filter_html: true),
-                                                   autolink: true,
-                                                   space_after_headers: true,
-                                                   tables: true,
-                                                   superscript: true)
+  def govspeak(text)
+    Govspeak::Document.new(text).to_sanitized_html.html_safe if text
   end
 end
