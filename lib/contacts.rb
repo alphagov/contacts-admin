ca2bd4230ba549938e0e079b668d43a1434ffed9
Mario Andrés Correa <admin@macool.me>
2014-01-30 12:16:59 -0500

    setting Contacts.organisations_api as an initializer

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,6 +4,7 @@ module Contacts
   mattr_accessor :mainstream_search_client
   mattr_accessor :contacts_search_client
   mattr_accessor :worldwide_api
+  mattr_accessor :organisations_api
 
   module_function
 end
