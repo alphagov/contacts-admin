067e9ad33379cb799f0524acc17c6e957c408d65
Mario Andrés Correa <admin@macool.me>
2014-02-03 10:29:09 -0500

    remove the grouping by contact group, just list them all.
    Also made templates simpler

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,31 +11,11 @@ class ContactsController < ApplicationController
     department.contacts.find_by!(slug: params[:id]).decorate
   }
 
-  expose(:most_popular_contact_groups) {
-    department.
-    contact_groups.
-    most_popular.
-    with_contacts.
-    merge(Contact.for_listing).
-    by_title.
-    decorate
-  }
-
-  expose(:ungrouped_contacts) {
+  expose(:contacts) {
     department.
     contacts.
     ungrouped.
     for_listing.
-    decorate
-  }
-
-  expose(:contact_groups) {
-    department.
-    contact_groups.
-    with_contacts.
-    except_most_popular.
-    by_title.
-    merge(Contact.for_listing).
     decorate
   }
 
