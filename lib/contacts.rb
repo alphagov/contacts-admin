6f162d2301981b4c9268fb48ec5e980c68bc28bc
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-03-14 12:10:33 +0000

    Add constrain for /admin route
    Sets a flag in development.rb to enable admin routes. This should be set
    to true on a backend box as part of the deploy process.
    
    https://www.pivotaltracker.com/story/show/67458214

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,6 +4,7 @@ module Contacts
   mattr_accessor :contacts_search_client
   mattr_accessor :worldwide_api
   mattr_accessor :organisations_api
+  mattr_accessor :enable_admin_routes
 
   module_function
 end
