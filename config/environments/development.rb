4791534cfd387c9601618726ed275ca0bca6a643
Matthew Ford <matt@bitzesty.com>
2014-03-21 16:06:39 +0000

    Use WHITEHALL_API and RUMMAGER_API env variables to hit local services
    Instead of commenting out the development config settings use env variables
    Uses plek to find the services

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,19 @@ Contacts::Application.configure do
 
   config.after_initialize do
     Contacts.enable_admin_routes = true
-    # Comment out the following to hit the actual APIs
+    
     Contacts.worldwide_api = GdsApi::Worldwide.new("https://www.gov.uk")
-    Contacts.organisations_api = MockOrganisationsApi.new
-    Contacts.rummager_client = FakeRummageableIndex.new("http://localhost", 'mainstream', logger: Rails.logger)
+    
+    Contacts.organisations_api = if ENV['WHITEHALL_API']
+      GdsApi::Organisations.new( Plek.current.find('whitehall-admin') )
+    else
+      MockOrganisationsApi.new
+    end
+
+    Contacts.rummager_client = if ENV['RUMMAGER_API']
+      Rummageable::Index.new( Plek.current.find('search'), 'mainstream', logger: Rails.logger
+    else
+      FakeRummageableIndex.new("http://localhost", 'mainstream', logger: Rails.logger)
+    end
   end
 end
