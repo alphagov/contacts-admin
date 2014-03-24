e705b3d3488e8a065f3d1996b9c809bf525acd36
Matthew Ford <matt@bitzesty.com>
2014-03-24 13:38:45 +0000

    JS syntax

--- app/assets/javascripts/admin/common.js
+++ app/assets/javascripts/admin/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".js-select2").select2({allowClear: true})
+  $(".js-select2").select2({allowClear: true});
 
   $("section.diff").each(function( index ) {
     GOVUK.diff($(this).attr("id"));
