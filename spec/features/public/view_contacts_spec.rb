a4827f6475142e0156f896de94bf95319c81c468
Mario Andrés Correa <a@macool.me>
2014-03-25 13:16:04 -0500

    specs for filtering contacts

--- spec/features/public/view_contacts_spec.rb
+++ spec/features/public/view_contacts_spec.rb
@@ -11,4 +11,20 @@ describe "Contacts" do
     ensure_on contacts_path(Department.first)
     verify contacts_exist([contact, contact2])
   end
+
+  specify "should filter contacts", js: true do
+    # contacts for listing
+    contact = create(:phone_number).contact
+    contact2 = create(:phone_number).contact
+
+    visit contacts_path(Department.first)
+
+    fill_in "Contains", with: contact.title # filter
+
+    page.should have_content(contact.title)
+    page.should have_content(contact.description)
+
+    page.should_not have_content(contact2.title)
+    page.should_not have_content(contact2.description)
+  end
 end
