44d654ff9845a1f9d21cc6ce137960b8a7e71866
Mario Andrés Correa <a@macool.me>
2014-03-26 11:41:12 -0500

    make specs work without javascript

--- spec/features/public/view_contacts_spec.rb
+++ spec/features/public/view_contacts_spec.rb
@@ -12,27 +12,36 @@ describe "Contacts" do
     it { verify contacts_exist([contact, contact2]) }
   end
 
-  context "filter", js: true do
+  context "filter" do
+    let(:refresh_results) { click_on "Refresh results" }
+
     context "by title" do
-      before { fill_in "Contains", with: contact.title }
-
-      it { verify contacts_exist([contact]) }
-      it { expect(page).to_not have_content(contact2.title) }
-    end
-    context "by topic" do
-      before { select contact.contact_groups.first.to_s, from: "Topic" }
-
-      it { verify contacts_exist([contact]) }
-      it { expect(page).to_not have_content(contact2.title) }
-    end
-    context "by both title and topic" do
-      before { 
+      before {
         fill_in "Contains", with: contact.title
-        select contact.contact_groups.first.to_s, from: "Topic"
+        refresh_results
       }
 
-      it { verify contacts_exist([contact]) }
-      it { expect(page).to_not have_content(contact2.title) }
+      it { should_list_contact(contact) }
+      it { should_not_list_contact(contact2) }
+    end
+    context "by topic" do
+      before {
+        select contact.contact_groups.first.to_s, from: "Topic"
+        refresh_results
+      }
+
+      it { should_list_contact(contact) }
+      it { should_not_list_contact(contact2) }
+    end
+    context "by both title and topic" do
+      before {
+        fill_in "Contains", with: contact.title
+        select contact.contact_groups.first.to_s, from: "Topic"
+        refresh_results
+      }
+
+      it { should_list_contact(contact) }
+      it { should_not_list_contact(contact2) }
     end
   end
 end
