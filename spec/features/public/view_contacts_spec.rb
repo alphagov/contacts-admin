53535a6a9e44a7e5904ec2598a7ac00ccb7b41ab
Matthew Ford <matt@bitzesty.com>
2014-03-26 14:07:16 +0000

    Extend the contacts filtering specs to include filter by topic

--- spec/features/public/view_contacts_spec.rb
+++ spec/features/public/view_contacts_spec.rb
@@ -3,10 +3,9 @@ require "spec_helper"
 describe "Contacts" do
   include Public::ContactSteps
 
-  let!(:contact) { create(:phone_number).contact }
-  let!(:contact2) { create(:phone_number).contact }
+  let!(:contact) { create(:contact, :with_phone_numbers, :with_contact_group) }
+  let!(:contact2) { create(:contact, :with_phone_numbers, :with_contact_group) }
 
-  before { Department.first.contacts.count.should eq(2) }
   before { ensure_on contacts_path(Department.first) }
 
   context "list" do
@@ -14,9 +13,26 @@ describe "Contacts" do
   end
 
   context "filter", js: true do
-    before { fill_in "Contains", with: contact.title } # filter
+    context "by title" do
+      before { fill_in "Contains", with: contact.title }
 
-    it { verify contacts_exist([contact]) }
-    it { expect(page).to_not have_content(contact2.title) }
+      it { verify contacts_exist([contact]) }
+      it { expect(page).to_not have_content(contact2.title) }
+    end
+    context "by topic" do
+      before { select contact.contact_groups.first, from: "Topic" }
+
+      it { verify contacts_exist([contact]) }
+      it { expect(page).to_not have_content(contact2.title) }
+    end
+    context "by both title and topic" do
+      before { 
+        fill_in "Contains", with: contact.title
+        select contact.contact_groups.first, from: "Topic"
+      }
+
+      it { verify contacts_exist([contact]) }
+      it { expect(page).to_not have_content(contact2.title) }
+    end
   end
 end
