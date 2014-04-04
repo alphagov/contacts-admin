a7277d156ff0e8f077374a62074c2adb5d50efc9
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-04-04 10:54:23 +0100

    Slimmer just includes the header and footer js
    While debugging another issue, @edds noticed that there was a lot of
    js being included that wasn't needed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   after_filter :dev_skip_slimmer
+  before_filter :set_slimmer_headers
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
@@ -18,5 +19,10 @@ class ApplicationController < ActionController::Base
   def skip_slimmer
     response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
   end
+
+   def set_slimmer_headers
+    response.headers[Slimmer::Headers::TEMPLATE_HEADER] = "header_footer_only"
+  end
+
 
 end
