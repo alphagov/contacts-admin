e453a9cb281da9dc973c5a0590df4f4d3ae99f19
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-07-02 16:06:59 +0000

    Refactor FE routes to remove confusing scoping.
    This was overly complicated for what boiled down to basically 2 get
    routes and a couple of redirects.

--- config/routes.rb
+++ config/routes.rb
@@ -25,18 +25,12 @@ Contacts::Application.routes.draw do
       end
     end
   end
-    
-  scope :path => "#{APP_SLUG}" do
 
-    scope ':organisation_slug' do
-      get "/", to: "contacts#index", as: :contacts
 
-      resources :contacts, constraints: { id: SLUG_FORMAT }, path: '/'
-    end
+  get "/#{APP_SLUG}/:organisation_slug" => "contacts#index", :as => :contacts
+  get "/#{APP_SLUG}/:organisation_slug/:id" => "contacts#show", :as => :contact, :constraints => {:id => SLUG_FORMAT }
 
-    # DEFAULT TO HMRC
-    get "/", to: redirect("/#{APP_SLUG}/hm-revenue-customs", status: 302)
-  end
-
-  root to: redirect("/#{APP_SLUG}", status: 302)
+  # DEFAULT TO HMRC
+  get "/#{APP_SLUG}" => redirect("/#{APP_SLUG}/hm-revenue-customs", status: 302)
+  root :to => redirect("/#{APP_SLUG}/hm-revenue-customs", status: 302)
 end
