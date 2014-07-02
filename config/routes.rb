d3fb61b36d4597eef634cf67a2729822423269d8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-07-02 17:21:54 +0100

    Support new contacts URLs
    Contact pages are going to be served from
    /government/organisations/<org_slug>/contact instead of the original url
    of /contact/<org_slug>. This change makes this app support both schemes.
    The old one will be removed once everything has switched over.

--- config/routes.rb
+++ config/routes.rb
@@ -26,11 +26,12 @@ Contacts::Application.routes.draw do
     end
   end
 
+  get "/government/organisations/:organisation_slug/contact" => "contacts#index", :as => :contacts
+  get "/government/organisations/:organisation_slug/contact/:id" => "contacts#show", :as => :contact, :constraints => {:id => SLUG_FORMAT }
 
-  get "/#{APP_SLUG}/:organisation_slug" => "contacts#index", :as => :contacts
-  get "/#{APP_SLUG}/:organisation_slug/:id" => "contacts#show", :as => :contact, :constraints => {:id => SLUG_FORMAT }
+  get "/contact/:organisation_slug" => "contacts#index", :as => :legacy_contacts
+  get "/contact/:organisation_slug/:id" => "contacts#show", :as => :legacy_contact, :constraints => {:id => SLUG_FORMAT }
 
   # DEFAULT TO HMRC
-  get "/#{APP_SLUG}" => redirect("/#{APP_SLUG}/hm-revenue-customs", status: 302)
-  root :to => redirect("/#{APP_SLUG}/hm-revenue-customs", status: 302)
+  root :to => redirect("/government/organisations/hm-revenue-customs/contact", status: 302)
 end
