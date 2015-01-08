470bd70956e52643cc4281eea32e8c681fec1c10
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-08 13:56:39 +0000

    Fix broken frontend assets
    Setting the asset_host to nil in the admin controller was intended to only
    apply to admin, but actually applied to frontend too. This broke the links to
    stylesheets in the frontend in production-like environments.
    
    I copied the technique from here:
    http://stackoverflow.com/a/10252239

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,6 @@
 class AdminController < ApplicationController
   layout "admin"
 
-  # Don't serve admin assets from the CDN
-  # Respond.js needs to run on the same domain to request stylesheets,
-  # parse them and render a non-mobile layout on <IE9
-  # https://github.com/scottjehl/Respond#cdnx-domain-setup
-  ActionController::Base.asset_host = nil
-
   prepend_before_filter :authenticate_user!
   before_filter :require_signin_permission!
   before_filter :skip_slimmer
