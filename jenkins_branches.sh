5e19dc0f04741ebb0ce12ed8033e00b30cd0e133
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-09 14:37:00 +0000

    Update app repo in jenkins_branches.sh
    The GitHub repo has moved and I think this mismatch is causing the branch
    builds to fail.

--- jenkins_branches.sh
+++ jenkins_branches.sh
@@ -8,7 +8,7 @@ VENV_PATH="${HOME}/venv/${JOB_NAME}"
 
 pip install -q ghtools
 
-REPO="alphagov/hmrc-contacts"
+REPO="alphagov/contacts-admin"
 gh-status "$REPO" "$GIT_COMMIT" pending -d "\"Build #${BUILD_NUMBER} is running on Jenkins\"" -u "$BUILD_URL" >/dev/null
 
 if ./jenkins.sh; then
