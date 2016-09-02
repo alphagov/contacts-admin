0c132ccc817d7b98d9913fe23ea894dace2a785c
David Basalla <davidbasalla@gmail.com>
2016-09-02 16:22:37 +0100

    Add scheduled task
    We move this out of alphagov_deployment in order to move to new way of deploying this app

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,1 +1,11 @@
-# This file is written on deploy
+# default cron env is "/usr/bin:/bin" which is not sufficient as govuk_env is in /usr/local/bin
+env :PATH, "/usr/local/bin:/usr/bin:/bin"
+
+# We need Rake to use our own environment
+job_type :rake, "cd :path && govuk_setenv contacts bundle exec rake :task :output"
+
+set :output, {:error => "log/cron.error.log", :standard => "log/cron.log"}
+
+every :day, at: "3am" do
+  rake "organisations:import"
+end
