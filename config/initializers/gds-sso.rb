d9128a9ff351f15035ffab9dcb15e78882c915de
David Basalla <davidbasalla@gmail.com>
2016-09-19 14:37:08 +0000

    Update sso env vars
    These env vars now follow the convention of other GOVUK apps and are set in govuk-puppet

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = ENV['HMRC_CONTACTS_OAUTH_ID'] || "abcdefghjasndjkasndcontacts"
-  config.oauth_secret = ENV['HMRC_CONTACTS_OAUTH_SECRET'] || "secret"
+  config.oauth_id     = ENV['OAUTH_ID'] || "abcdefghjasndjkasndcontacts"
+  config.oauth_secret = ENV['OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.new.find("signon")
 end
