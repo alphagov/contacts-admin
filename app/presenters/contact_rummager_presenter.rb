801d4b35061405cbc1dd6cef43c68d80e5721185
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2016-11-01 11:21:00 +0000

    Migrate the HMRC contacts page to finder-frontend
    This commit migrates the rendering of the HMRC contacts page at https://www.gov.uk/government/organisations/hm-revenue-customs/contact from contacts-admin to finder-frontend, since the formats are so similar.
    
    A service and rake task are provided to publish the finder to the Publishing API. This also happens every time a contact group is added/edited/deleted in the admin interface since the contact groups are included in the JSON that is sent to the Publishing API.
    
    Finally, contact groups are sent to rummager to be indexed so that the finder facets work.

--- app/presenters/contact_rummager_presenter.rb
+++ app/presenters/contact_rummager_presenter.rb
@@ -13,6 +13,7 @@ class ContactRummagerPresenter
       format: "contact",
       indexable_content: "#{contact.title} #{contact.description} #{contact.contact_groups.map(&:title).join}",
       public_timestamp: contact.updated_at,
+      contact_groups: contact.contact_groups.map(&:slug)
     }
   end
 end
---
+++ app/services/publish_finders.rb
@@ -0,0 +1,72 @@
+# This service currently only publishes the HMRC contacts finder since this app
+# only contains contacts related to HMRC.
+class PublishFinders
+  def self.call
+    new.call
+  end
+
+  def call
+    Services.publishing_api.put_content(
+      HMRC_CONTACTS_CONTENT_ID,
+      hmrc_contacts_payload
+    )
+    Services.publishing_api.publish(HMRC_CONTACTS_CONTENT_ID, "major")
+  end
+
+private
+
+  HMRC_CONTACTS_CONTENT_ID = "b110c03c-3f8d-4327-906b-17ebd872e6a6".freeze
+
+  def hmrc_contacts_payload
+    {
+      "base_path": "/government/organisations/hm-revenue-customs/contact",
+      "title": "HM Revenue & Customs Contacts",
+      "description": "",
+      "locale": "en",
+      "document_type": "finder",
+      "schema_name": "finder",
+      "publishing_app": "contacts",
+      "rendering_app": "finder-frontend",
+      "details": {
+        "document_noun": "contact",
+        "default_order": "title",
+        "facets": [
+          {
+            "key": "contact_groups",
+            "name": "Topics",
+            "type": "text",
+            "preposition": "in topic",
+            "display_as_result_metadata": false,
+            "filterable": true,
+            "allowed_values": contact_groups_hash
+          }
+        ],
+        "filter": {
+          "format": "contact"
+        },
+        "format_name": "HM Revenue & Customs Contacts",
+        "show_summaries": true
+      },
+      "routes": [
+        {
+          "type": "exact",
+          "path": "/government/organisations/hm-revenue-customs/contact"
+        },
+        {
+          "type": "exact",
+          "path": "/government/organisations/hm-revenue-customs/contact.json"
+        }
+      ]
+    }
+  end
+
+  def contact_groups_hash
+    contact_groups = ContactGroup.order("title ASC").all
+    contact_groups.map do |contact_group|
+      {
+        label: contact_group.title,
+        value: contact_group.slug
+      }
+    end
+  end
+end
