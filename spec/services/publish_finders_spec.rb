725c9b3fe1de64d4aaed61c549ee850781e0a28e
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2016-11-07 10:40:22 +0000

    Set the parent of the HMRC contacts finder
    This commit sets the parent of the HMRC contacts finder to the HMRC organisation page. This will allow finder-frontend to display the correct breadcrumbs for the finder, once a corresponding change has been made to that app.

--- spec/services/publish_finders_spec.rb
+++ spec/services/publish_finders_spec.rb
@@ -4,12 +4,23 @@ RSpec.describe PublishFinders do
   before do
     @hmrc_contacts_content_id = PublishFinders::HMRC_CONTACTS_CONTENT_ID
     @hmrc_contacts_finder = PublishFinders.new.send(:hmrc_contacts_payload)
+    @hmrc_contacts_parent = PublishFinders.new.send(:hmrc_contacts_parent)
   end
 
   it 'publishes the HMRC contacts finder' do
     PublishFinders.call
 
-    assert_publishing_api_put_content(@hmrc_contacts_content_id, @hmrc_contacts_finder)
-    assert_publishing_api_publish(@hmrc_contacts_content_id, update_type: 'major')
+    assert_publishing_api_put_content(
+      @hmrc_contacts_content_id,
+      @hmrc_contacts_finder
+    )
+    assert_publishing_api_publish(
+      @hmrc_contacts_content_id,
+      update_type: 'major'
+    )
+    assert_publishing_api_patch_links(
+      @hmrc_contacts_content_id,
+      links: @hmrc_contacts_parent
+    )
   end
 end
