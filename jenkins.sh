412ff30dd3f3d7307d3a15d0dbdd2aa8c67618e5
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-11-22 14:10:44 +0000

    Change default govuk-content-schemas branch
    Change the branch from master to deployed-to-production. This will help
    prevent changes being merged in to the Contacts Admin app that don't
    work with the currently deployed govuk-content-schemas (this changes
    comes from GOV.UK RFC 59).

--- jenkins.sh
+++ jenkins.sh
@@ -9,7 +9,7 @@ bundle exec rake assets:precompile --trace
 
 # Clone govuk-content-schemas depedency for contract tests
 rm -rf tmp/govuk-content-schemas
-git clone git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
+git clone --branch deployed-to-production git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
 
 GOVUK_CONTENT_SCHEMAS_PATH=tmp/govuk-content-schemas COVERAGE=on bundle exec rake ci:setup:rspec spec --trace
 
