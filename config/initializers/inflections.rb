7e700e870627f04e4836c7f955f5b1ba3a90c199
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-05-02 15:47:01 +0000

    Suggested changes from `rails app:upgrade`

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,11 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
- ActiveSupport::Inflector.inflections(:en) do |inflect|
-   inflect.acronym 'UUID'
- end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'UUID'
+end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
