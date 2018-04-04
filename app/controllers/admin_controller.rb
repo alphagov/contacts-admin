baf1ee97891233573328283dfd71c37d59d16068
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-04-04 15:11:54 +0100

    Remove require_signin_permission!
    This is now redundant and deprecated.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,6 @@ class AdminController < ApplicationController
   layout "admin"
 
   prepend_before_action :authenticate_user!
-  before_action :require_signin_permission!
 
   def info_for_paper_trail
     { user_name: current_user.name }
