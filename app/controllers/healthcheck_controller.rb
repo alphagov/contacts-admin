8f27a5da2f735d67c1ba2c336f05e40c0b5c6d5c
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-08-02 08:01:20 +0100

    Make the health check controller an API controller
    This means we won't get warnings about not protecting from a CSRF
    attack.

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -1,4 +1,4 @@
-class HealthcheckController < ActionController::Base
+class HealthcheckController < ActionController::API
   def check
     Contact.first # check DB connectivity
     render json: { git_sha1: CURRENT_RELEASE_SHA }
