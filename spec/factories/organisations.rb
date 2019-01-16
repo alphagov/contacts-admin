67b113338900e2bdcc443c62b7f3b8b16722833f
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-01-16 18:59:24 +0000

    Remove FactoryBot static attributes
    As these will be removed in FactoryBot 5.0.

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
   factory :organisation do
     sequence(:title) { |n| "Organisation #{n}" }
     slug { title.parameterize }
-    format "Non-ministerial department"
+    format { "Non-ministerial department" }
     sequence(:abbreviation) { |n| "ORG#{n}" }
-    govuk_status "live"
+    govuk_status { "live" }
     content_id { |_n| SecureRandom.uuid }
-    contact_index_content_id SecureRandom.uuid
+    contact_index_content_id { SecureRandom.uuid }
   end
 end
