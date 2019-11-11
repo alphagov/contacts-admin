2c27e8ffa9ef62bb2d5a040911438ca67e25745b
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-11-11 11:40:17 +0000

    Set assets.export_concurrent to false
    There seems to be issues with Sprockets version 4, errors like [1]
    occur when precompiling assets.
    
    A suggestion I've seen to avoid this is to add this configuration [2].
    
    1: SassC::SyntaxError: Error: error in C function font-path: undefined
    method `[]' for nil:NilClass
    2: https://github.com/rails/sprockets/issues/581#issuecomment-486984663

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+# TODO: This seems to help work around asset precompilation failures
+Rails.application.config.assets.export_concurrent = false
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
