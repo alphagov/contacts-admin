3df28771bed9ea6943b27fe5e778eea88ac55737
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-07-22 09:53:19 +0100

    Remove user validations
    This was added in cf5e9dc685304f62c42727206eee8d01df02ebf3 but it
    conflicts with the latest version of GDS SSO. I can't find any other
    application which has this line, even ones that send emails, so I don't
    think we need it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,4 @@ class User < ApplicationRecord
   include GDS::SSO::User
 
   serialize :permissions, Array
-
-  validates :name, presence: true
-  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,4 @@ require "gds-sso/lint/user_spec"
 
 RSpec.describe User, type: :model do
   it_behaves_like "a gds-sso user class"
-
-  it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_presence_of :email }
 end
