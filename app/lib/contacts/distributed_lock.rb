24bc1762d645e0e887d06ba82eb0a0c21b3ad646
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-02-05 09:05:34 +0000

    Remove SQL injection attack vector
    Brakeman is warning of a possible exploitable SQL injection:
    
    ```
    Confidence: Weak
    Category: SQL Injection
    Check: SQL
    Message: Possible SQL injection
    Code: Redis.current.lock("contacts-admin:#{Rails.env}:#{lock_name}", :life => (LIFETIME))
    File: app/lib/contacts/distributed_lock.rb
    Line: 13
    ```
    
    A search of contacts-admin shows that `Contacts::DistributedLock`
    is only used by the `organisations:import` Rake task, which runs
    every day at 3am (according to config/schedule.rb). Therefore a
    parameterised lock name is not needed - we can simply hardcode the
    lock name to the value we know it will be.
    
    We can take this a step further by removing the `Rails.env`
    portion of the lock name, as we have different Redis instances in
    different environments, so there's no need to namespace by env.

--- app/lib/contacts/distributed_lock.rb
+++ app/lib/contacts/distributed_lock.rb
@@ -5,17 +5,13 @@ module Contacts
   class DistributedLock
     LIFETIME = (5 * 60) # seconds. The lock expires automatically after this time
 
-    def initialize(lock_name)
-      @lock_name = lock_name
-    end
-
     def lock
-      Redis.current.lock("contacts-admin:#{Rails.env}:#{@lock_name}", life: LIFETIME) do
+      Redis.current.lock("contacts-admin:organisations:import", life: LIFETIME) do
         Rails.logger.debug("Successfully got a lock. Running...")
         yield
       end
     rescue Redis::Lock::LockNotAcquired => e
-      Rails.logger.debug("Failed to get lock for #{@lock_name} (#{e.message}). Another process probably got there first.")
+      Rails.logger.debug("Failed to get lock for organisations:import (#{e.message}). Another process probably got there first.")
     end
   end
 end
