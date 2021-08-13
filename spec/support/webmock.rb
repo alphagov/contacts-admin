a14065811a46ea3d99e3aef6d44935ee677d1d43
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-08-13 11:28:36 +0100

    Trying something  else

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,3 +1,10 @@
 require "webmock/rspec"
 
 WebMock.disable_net_connect!(allow_localhost: true)
+
+require "govuk-content-schema-test-helpers/test_unit"
+
+GovukContentSchemaTestHelpers.configure do |config|
+  config.schema_type = "publisher_v2"
+  config.project_root = Rails.root
+end
