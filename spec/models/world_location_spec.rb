08b2d0f8abd51be700d4ab3026fd03963f820db2
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2022-04-20 11:48:37 +0100

    Improve test for WorldLocation#==

--- spec/models/world_location_spec.rb
+++ spec/models/world_location_spec.rb
@@ -18,9 +18,19 @@ describe WorldLocation, type: :model do
   describe "#==" do
     let(:wl1) { WorldLocation.new({ "details" => { "slug" => "Neverland" } }) }
     let(:wl2) { WorldLocation.new({ "details" => { "slug" => "Neverland" } }) }
+    let(:wl3) { WorldLocation.new({ "details" => { "slug" => "Narnia" } }) }
+    let(:not_wl) { create(:organisation, slug: "Neverland") }
 
     it "returns true if the locations have the same slug" do
       expect(wl1 == wl2).to be true
+    end
+
+    it "returns false if the locations have different slugs" do
+      expect(wl1 == wl3).to be false
+    end
+
+    it "returns false if compared to something other than a WorldLocation" do
+      expect(wl1 == not_wl).to be false
     end
   end
 end
