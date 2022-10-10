918e5daf9b8438312f0157eff2b8f2bc3768b62a
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-10-10 15:47:44 +0100

    wip

--- spec/lib/contacts/distributed_lock_spec.rb
+++ spec/lib/contacts/distributed_lock_spec.rb
@@ -4,13 +4,19 @@ require "redis-lock"
 
 RSpec.describe "Contacts::DistributedLock" do
   describe "#lock" do
+    let(:redis_instance) { instance_double(Redis, lock: {}) }
+
+    before(:each) do
+      allow(Redis).to receive(:new).and_return(redis_instance)
+    end
+
     it "yields the block when a redis lock can be acquired" do
-      expect_any_instance_of(Redis).to receive(:lock).and_call_original
+      expect(redis_instance).to receive(:lock)
       expect { |b| Contacts::DistributedLock.new.lock(&b) }.to yield_control
     end
 
     it "silently doesn't a yield the block when a lock can't be acquired" do
-      allow_any_instance_of(Redis).to receive(:lock).and_raise(Redis::Lock::LockNotAcquired)
+      allow(redis_instance).to receive(:lock).and_raise(Redis::Lock::LockNotAcquired)
       expect { |b| Contacts::DistributedLock.new.lock(&b) }.not_to yield_control
     end
   end
