6c8f728aaa8a5a1db88a54935012659b6cfa0961
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-05-28 15:05:01 +0100

    Add exception handling for if no redirect URL is provided

--- app/services/contact_migrator.rb
+++ app/services/contact_migrator.rb
@@ -3,10 +3,12 @@
 
 module MigrateContactError
   class ContactNotFound < StandardError; end
+  class RedirectUrlMissing < StandardError; end
 end
 
 class ContactMigrator
   def migrate_contact(original_url: nil, new_url: nil)
+    validate_redirect_url!(original_url, new_url)
     contact = contact_by_url(original_url)
 
     Services.publishing_api.unpublish(
@@ -19,6 +21,10 @@ class ContactMigrator
   end
 
 private
+
+  def validate_redirect_url!(original_url, new_url)
+    raise MigrateContactError::RedirectUrlMissing, "Missing a redirect URL (`new_url`) for #{original_url}" unless new_url
+  end
 
   def contact_by_url(url)
     slug = url.split("/").last
--- spec/services/contact_migrator_spec.rb
+++ spec/services/contact_migrator_spec.rb
@@ -2,14 +2,12 @@ require "rails_helper"
 
 RSpec.describe ContactMigrator do
   describe "#migrate_contact" do
+    let(:original_url) { "/government/organisations/hm-revenue-customs/contact/corporation-tax-enquiries" }
     let(:new_url) { "/find-hmrc-contacts/corporation-tax-general-enquiries" }
 
     it "makes an unpublish request to Publishing API for the contact" do
       contact = create(:contact, slug: "corporation-tax-enquiries")
-      ContactMigrator.new.migrate_contact(
-        original_url: "/government/organisations/hm-revenue-customs/contact/corporation-tax-enquiries",
-        new_url:,
-      )
+      ContactMigrator.new.migrate_contact(original_url:, new_url:)
 
       assert_publishing_api_unpublish(
         contact.content_id,
@@ -30,6 +28,15 @@ RSpec.describe ContactMigrator do
       expect { ContactMigrator.new.migrate_contact(**bad_args) }.to raise_error(
         MigrateContactError::ContactNotFound,
         "No corresponding Contact found for #{typo_url}",
+      )
+    end
+
+    it "errors if no redirect URL provided" do
+      create(:contact, slug: "corporation-tax-enquiries")
+
+      expect { ContactMigrator.new.migrate_contact(original_url:) }.to raise_error(
+        MigrateContactError::RedirectUrlMissing,
+        "Missing a redirect URL (`new_url`) for #{original_url}",
       )
     end
   end
