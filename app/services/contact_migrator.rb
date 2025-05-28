509aa963a99bde4a4bf74f2488c470d31bc5f69d
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-05-28 14:57:09 +0100

    Add exception handling for if there is no Contact for provided URL

--- app/services/contact_migrator.rb
+++ app/services/contact_migrator.rb
@@ -1,6 +1,10 @@
 # Temporary class to migrate an individual contact from its Contacts Admin
 # page to its HMRC Specialist Finder equivalent
 
+module MigrateContactError
+  class ContactNotFound < StandardError; end
+end
+
 class ContactMigrator
   def migrate_contact(original_url: nil, new_url: nil)
     contact = contact_by_url(original_url)
@@ -18,6 +22,8 @@ private
 
   def contact_by_url(url)
     slug = url.split("/").last
-    Contact.find_by(slug:)
+    Contact.find_by!(slug:)
+  rescue ActiveRecord::RecordNotFound
+    raise MigrateContactError::ContactNotFound, "No corresponding Contact found for #{url}"
   end
 end
--- spec/services/contact_migrator_spec.rb
+++ spec/services/contact_migrator_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 RSpec.describe ContactMigrator do
   describe "#migrate_contact" do
+    let(:new_url) { "/find-hmrc-contacts/corporation-tax-general-enquiries" }
+
     it "makes an unpublish request to Publishing API for the contact" do
       contact = create(:contact, slug: "corporation-tax-enquiries")
-      new_url = "/find-hmrc-contacts/corporation-tax-general-enquiries"
       ContactMigrator.new.migrate_contact(
         original_url: "/government/organisations/hm-revenue-customs/contact/corporation-tax-enquiries",
         new_url:,
@@ -16,6 +17,19 @@ RSpec.describe ContactMigrator do
         explanation: "Migrated to HMRC Specialist Finder",
         alternative_path: new_url,
         discard_drafts: true,
+      )
+    end
+
+    it "errors if no contact can be found for the provided URL" do
+      typo_url = "/government/organisations/hm-revenue-customs/contact/corporation-tax-enquiries-typo"
+      bad_args = {
+        original_url: typo_url,
+        new_url:,
+      }
+
+      expect { ContactMigrator.new.migrate_contact(**bad_args) }.to raise_error(
+        MigrateContactError::ContactNotFound,
+        "No corresponding Contact found for #{typo_url}",
       )
     end
   end
